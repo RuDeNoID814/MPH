100fdec16036cf201acd86a4a0b15cbd49c2f453
MARON <rudenko@rud-m.ru>
2025-02-25 22:07:25 +0300

    MPH 1.1.2

--- scriptmain.js
+++ scriptmain.js
@@ -1,36 +1,5 @@
-// Кнопка "Начало работы"
-document.getElementById('start-work-btn').addEventListener('click', function() {
-    window.open('https://supchat.taxi.yandex-team.ru/settings', '_blank');
-    window.open('https://yandex.ru/maps/', '_blank');
-    window.open('https://delivery-admin-frontend-external.taxi.yandex-team.ru/corp-claims?external_order_id=307577825', '_blank');
-});
-// Меню с выходом и связью
-const menuIcon = document.getElementById('menu-icon');
-const menu = document.getElementById('menu');
-let timeout;
 
-menuIcon.addEventListener('mouseenter', function() {
-    clearTimeout(timeout);
-    menu.classList.add('show');
-});
-
-menuIcon.addEventListener('mouseleave', function() {
-    timeout = setTimeout(() => {
-        menu.classList.remove('show');
-    }, 300);
-});
-
-menu.addEventListener('mouseenter', function() {
-    clearTimeout(timeout);
-    menu.classList.add('show');
-});
-
-menu.addEventListener('mouseleave', function() {
-    timeout = setTimeout(() => {
-        menu.classList.remove('show');
-    }, 300);
-});
-
+// Код сброса флага авторизации и выход с сайта на index.html
 document.getElementById('logout-btn').addEventListener('click', function(event) {
     event.preventDefault();
     localStorage.removeItem('authenticated');
